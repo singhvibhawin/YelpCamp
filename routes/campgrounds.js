127642b8e7b9e3020b8e3c7e8d4199a61b063db1
Bhavin Singhvi <singhvibhavin@gmail.com>
2024-06-14 13:53:55 +0530

    Storing image in cloudianary

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,15 +4,14 @@ const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 const multer  = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const { storage } = require('../cloudinary') 
+const upload = multer({ storage });
+
 const Campground = require('../models/campground');
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
-    .post(upload.single('image'), (req, res) => {
-        res.send(req.body, req.file);
-    })
+    .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground))
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
